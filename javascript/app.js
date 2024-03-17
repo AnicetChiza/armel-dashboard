833ca2cb256cd93b0d004f22fd7a3324cc611d56
AnicetChiza <chizaanicet@gmail.com>
2024-03-17 13:46:00 +0200

    working on the login and connected pages

--- javascript/app.js
+++ javascript/app.js
@@ -3,3 +3,15 @@ let openIcon = document.querySelector('.open');
 let closeIcon = document.querySelector('.close');
 
 password.setAttribute('type', 'password');
+
+openIcon.onclick = function() {
+    password.setAttribute('type', 'text');
+    openIcon.classList.add('hidden');
+    closeIcon.classList.remove('hidden');
+}
+
+closeIcon.onclick = function() {
+    password.setAttribute('type', 'password');
+    openIcon.classList.remove('hidden');
+    closeIcon.classList.add('hidden');
+}
