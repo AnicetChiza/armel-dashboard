fbf654f035fde601eb0daeb85377badf32073d4d
AnicetChiza <chizaanicet@gmail.com>
2024-03-19 16:04:00 +0200

    i was working on the functionnality of displaying a popup in our management system once we need to delete a student, we have to choose among the part of deleting on cancel the process

--- javascript/popup.js
+++ javascript/popup.js
@@ -1,4 +1,12 @@
 const popup = document.querySelector('.popup');
 const deleteIcon = document.querySelectorAll('.delete');
 const cancelPopup = document.querySelector('.cancel-popup');
-const deletePopup = document.querySelector('.delete-popup');
+
+deleteIcon.forEach(deleteIcon => {
+    deleteIcon.onclick = function() {
+        popup.classList.remove('hidden-popup');
+    }
+});
+cancelPopup.onclick = function() {
+    popup.classList.add('hidden-popup');
+}
