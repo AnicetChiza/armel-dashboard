1b0ba6a64dffdc399a9cfb509ca671e1089b7693
AnicetChiza <chizaanicet@gmail.com>
2024-03-19 15:47:46 +0200

    add to my repository

--- javascript/popup.js
+++ javascript/popup.js
@@ -1,7 +1,4 @@
-const deleteBtn = document.querySelector('.delete');
-const cancelPopup = document.querySelector('.popup-cancel');
-const pop = document.querySelector('.popup');
-
-deleteBtn.onclick = function() {
-    pop.classList.remove('hidden');
-}
+const popup = document.querySelector('.popup');
+const deleteIcon = document.querySelectorAll('.delete');
+const cancelPopup = document.querySelector('.cancel-popup');
+const deletePopup = document.querySelector('.delete-popup');
